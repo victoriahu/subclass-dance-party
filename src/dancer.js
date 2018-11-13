20d2dc44a1f13fe080040ea5ddf02d17d6e0ea18
victoriahu <vh12490@gmail.com>
2018-11-12 16:20:11 -0800

    convert to functional to pseudoclassical

--- src/dancer.js
+++ src/dancer.js
@@ -1,19 +1,32 @@
 // Creates and returns a new dancer object that can step
 var makeDancer = function(top, left, timeBetweenSteps) {
-
-  var dancer = {};
-
+  //console.log(makeDancer.prototype);
+  //why does this.step() not work?
+  // this.prototype.step();
+  
   // use jQuery to create an HTML <span> tag
-  dancer.$node = $('<span class="dancer"></span>');
+  this.$node = $('<span class="dancer"></span>');
+  this.timeBetweenSteps = timeBetweenSteps;
+  
+  console.log(this);
+  makeDancer.prototype.step.call(this);
 
-  dancer.step = function() {
-    // the basic dancer doesn't do anything interesting at all on each step,
+  makeDancer.prototype.setPosition.call(this, top, left);
+
+
+};
+
+
+  makeDancer.prototype.step = function() {
+    // the basic Dancer doesn't do anything interesting at all on each step,
     // it just schedules the next step
-    setTimeout(dancer.step, timeBetweenSteps);
+    console.log("step is invoked");
+    //setTimeout(this.step, this.timeBetweenSteps);
+    setTimeout(this.step.bind(this), this.timeBetweenSteps);
   };
-  dancer.step();
+  
 
-  dancer.setPosition = function(top, left) {
+  makeDancer.prototype.setPosition = function(top, left) {
     // Use css top and left properties to position our <span> tag
     // where it belongs on the page. See http://api.jquery.com/css/
     //
@@ -21,12 +34,9 @@ var makeDancer = function(top, left, timeBetweenSteps) {
       top: top,
       left: left
     };
-    dancer.$node.css(styleSettings);
+    this.$node.css(styleSettings);
   };
 
   // now that we have defined the dancer object, we can start setting up important parts of it by calling the methods we wrote
   // this one sets the position to some random default point within the body
-  dancer.setPosition(top, left);
-
-  return dancer;
-};
+ 
